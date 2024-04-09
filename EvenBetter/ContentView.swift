dac938275a6ec9c177616a52ee51bbdde020eb7d
Mello Birkan <mgbirkan@icloud.com>
2024-04-09 14:38:05 -0300

    Initial Commit

---
+++ EvenBetter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EvenBetter
+//
+//  Created by Marcello Gonzatto Birkan on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EvenBetter/EvenBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvenBetterApp.swift
+//  EvenBetter
+//
+//  Created by Marcello Gonzatto Birkan on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EvenBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
