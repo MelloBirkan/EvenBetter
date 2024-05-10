a645faa534f48c946156c724984d6f5879a0c130
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-05-10 15:38:25 -0300

    `Feat:` Agora Salva o desafio atual mesmo fechando o app
    Foi todo código dos desafios re-escrito para o SwiftData

--- EvenBetter/EvenBetter/Models/Challenge.swift
+++ EvenBetter/EvenBetter/Models/Challenge.swift
@@ -6,29 +6,38 @@
 //
 
 import SwiftUI
+import SwiftData
 
-struct Challenge {
+@Model
+final class Challenge {
   let mood: MoodType
   let name: String
   let summary: String
-  let image: Image
+  let image: String
   
-  static let sampleData = Challenge(mood: .ansioso, name: "Meditar", summary: "A meditação", image: Image(.meditation2))
+  init(mood: MoodType, name: String, summary: String, image: String) {
+    self.mood = mood
+    self.name = name
+    self.summary = summary
+    self.image = image
+  }
+  
+  static let sampleData = Challenge(mood: .ansioso, name: "Meditar", summary: "A meditação", image: "meditation2")
   
   static let challengesData = [
-          Challenge(mood: .feliz, name: "Gratidão", summary: "Escreva 3 coisas pelas quais você é grato hoje", image: Image("writing")),
-          Challenge(mood: .feliz, name: "Dança", summary: "Dance sua música favorita por alguns minutos", image: Image("dancing")),
-          Challenge(mood: .triste, name: "Conversa", summary: "Ligue para um amigo ou familiar e desabafe", image: Image("phone")),
-          Challenge(mood: .triste, name: "Filme", summary: "Assista a um filme de comédia ou que te faça sentir bem", image: Image("movie")),
-          Challenge(mood: .ansioso, name: "Respiração", summary: "Faça exercícios de respiração profunda por 5 minutos", image: Image("meditation2")),
-          Challenge(mood: .ansioso, name: "Caminhada", summary: "Faça uma caminhada relaxante ao ar livre", image: Image("walking")),
-          Challenge(mood: .motivado, name: "Meta", summary: "Defina uma meta pessoal e planeje passos para alcançá-la", image: Image("target")),
-          Challenge(mood: .motivado, name: "Aprendizado", summary: "Aprenda algo novo sobre um tópico que te interessa", image: Image("learning")),
-          Challenge(mood: .bravo, name: "Pausa", summary: "Faça uma pausa, respire fundo e pense antes de reagir", image: Image("stopSign")),
-          Challenge(mood: .bravo, name: "Exercício", summary: "Faça exercícios físicos para liberar a tensão", image: Image("exercise")),
-          Challenge(mood: .cansado, name: "Descanso", summary: "Tire uma soneca de 20 minutos ou relaxe ouvindo música calma", image: Image("sleeping")),
-          Challenge(mood: .cansado, name: "Alongamento", summary: "Faça alguns alongamentos suaves para energizar o corpo", image: Image("stretching")),
-          Challenge(mood: .ansioso, name: "Yoga", summary: "Faça uma sessão de yoga de 15 minutos para acalmar a mente e o corpo", image: Image("yoga")),
-          Challenge(mood: .motivado, name: "Corrida", summary: "Faça uma corrida de 20 minutos para energizar o corpo e a mente", image: Image("running"))
+          Challenge(mood: .feliz, name: "Gratidão", summary: "Escreva 3 coisas pelas quais você é grato hoje", image: "writing"),
+          Challenge(mood: .feliz, name: "Dança", summary: "Dance sua música favorita por alguns minutos", image: "dancing"),
+          Challenge(mood: .triste, name: "Conversa", summary: "Ligue para um amigo ou familiar e desabafe", image: "phone"),
+          Challenge(mood: .triste, name: "Filme", summary: "Assista a um filme de comédia ou que te faça sentir bem", image: "movie"),
+          Challenge(mood: .ansioso, name: "Respiração", summary: "Faça exercícios de respiração profunda por 5 minutos", image: "meditation2"),
+          Challenge(mood: .ansioso, name: "Caminhada", summary: "Faça uma caminhada relaxante ao ar livre", image: "walking"),
+          Challenge(mood: .motivado, name: "Meta", summary: "Defina uma meta pessoal e planeje passos para alcançá-la", image: "target"),
+          Challenge(mood: .motivado, name: "Aprendizado", summary: "Aprenda algo novo sobre um tópico que te interessa", image: "learning"),
+          Challenge(mood: .bravo, name: "Pausa", summary: "Faça uma pausa, respire fundo e pense antes de reagir", image: "stopSign"),
+          Challenge(mood: .bravo, name: "Exercício", summary: "Faça exercícios físicos para liberar a tensão", image: "exercise"),
+          Challenge(mood: .cansado, name: "Descanso", summary: "Tire uma soneca de 20 minutos ou relaxe ouvindo música calma", image: "sleeping"),
+          Challenge(mood: .cansado, name: "Alongamento", summary: "Faça alguns alongamentos suaves para energizar o corpo", image: "stretching"),
+          Challenge(mood: .ansioso, name: "Yoga", summary: "Faça uma sessão de yoga de 15 minutos para acalmar a mente e o corpo", image: "yoga"),
+          Challenge(mood: .motivado, name: "Corrida", summary: "Faça uma corrida de 20 minutos para energizar o corpo e a mente", image: "running")
       ]
 }
